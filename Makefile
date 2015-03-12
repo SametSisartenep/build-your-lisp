397a8e34ceed0f3453fabef6b3813ab7ae021061
SametSisartenep <krashbuffer@gmail.com>
2015-03-12 03:01:27 +0100

    Bit REPL v4 released (yes, released, although hard it's true)

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
 CC=clang
 CFLAGS= -std=c11 -leditline
+OUTPUT= -o bit-repl
 .PHONY=clean
 
+all: v4
+
 v1: bit-repl.c
-	$(CC) $(CFLAGS) -o bit-repl bit-repl.c
+	$(CC) $(CFLAGS) $(OUTPUT) bit-repl.c
 
 v2: bit-repl2.c
-	$(CC) $(CFLAGS) -o bit-repl bit-repl2.c
+	$(CC) $(CFLAGS) $(OUTPUT) bit-repl2.c
 
 v3: bit-repl3.c
-	$(CC) $(CFLAGS) -o bit-repl bit-repl3.c
+	$(CC) $(CFLAGS) $(OUTPUT) bit-repl3.c
+
+v4: bit-repl4.c
+	$(CC) $(CFLAGS) $(OUTPUT) bit-repl4.c
 
 clean:
 	rm -rfv ./bit-repl
---
+++ bit-repl4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <editline.h>
+
+int main ( int argc, char *argv[] ) {
+  char *input;
+
+  puts("Bit REPL - Version 0.0.4");
+  puts("Press Ctrl-c to Exit\n");
+
+  while (1) {
+    input = readline("bit-repl> ");
+
+    if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
+      printf("\nThanks for using Bit REPL :)\nClosing...\n");
+      free(input);
+      exit(1);
+    }
+
+    add_history(input);
+
+    printf("STDOUT: %s\n", input);
+  }
+
+  free(input);
+  return 0;
+}
